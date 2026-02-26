2a283f6c6fc6f157d384d7e6499f2cdbba648bfc
Jbayne71 <jalenbayne@gmail.com>
2026-02-25 22:39:10 -0500

    Added Ui for filter and implemented filter logic
    Set up filter Ui to sort by cost, prep time and difficulty, as well as filter by dietary tags. Recipes are displayed or removed based on selected options

--- src/frontend/src/pages/RecipeResultPage.jsx
+++ src/frontend/src/pages/RecipeResultPage.jsx
@@ -3,15 +3,49 @@ import { templateRecipes } from "../data/templateRecipes.js";
 import RecipeResult from "../components/RecipeResult.jsx";
 import { filterRecipes } from "../services/filterRecipes.js";
 import { useState } from "react";
+import RecipeFilterUI from "../components/RecipeFilterUI.jsx";
 
 function ReceipeResultPage() {
   const [searchQuery, setSearchQuery] = useState("");
-  const filteredRecipes = filterRecipes(templateRecipes, searchQuery);
+  const [prepTime, setPrepTime] = useState("");
+  const [difficulty, setDifficulty] = useState("");
+  const [cost, setCost] = useState("");
+  const [dietaryTags, setDietaryTags] = useState([]);
+
+  const handleReset = () => {
+    setSearchQuery("");
+    setPrepTime("");
+    setDifficulty("");
+    setCost("");
+    setDietaryTags([]);
+  };
+
+  const filteredRecipes = filterRecipes(
+    templateRecipes,
+    searchQuery,
+    prepTime,
+    difficulty,
+    cost,
+    dietaryTags
+  );
+
   return (
     <>
       <div style={{ margin: "auto", width: "100%" }}>
         <SearchBar onSearch={setSearchQuery} />
-
+        <RecipeFilterUI
+          searchQuery={searchQuery}
+          setSearchQuery={setSearchQuery}
+          prepTime={prepTime}
+          setPrepTime={setPrepTime}
+          difficulty={difficulty}
+          setDifficulty={setDifficulty}
+          cost={cost}
+          setCost={setCost}
+          dietaryTags={dietaryTags}
+          setDietaryTags={setDietaryTags}
+          handleReset={handleReset}
+        />
         <RecipeResult recipes={filteredRecipes} />
       </div>
     </>
